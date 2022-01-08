f7c107661a25c3c22049518361d7dd01541d0987
DenisFer-13 <denof59@gmail.com>
2022-01-07 21:11:09 -0300

    Fixed format

--- components/Container.js
+++ components/Container.js
@@ -1,11 +1,11 @@
 //Este será un contenedor que se renderizará en todos los componentes usando la propiedad children.
-import Navigation from "./navigation";
+import Navigation from "./navigation";        //Importamos el componente de navegación.
 
-const Container = (props) => (
+const Container = (props) => (                //Aquí generamos la función
   <div>
-    <Navigation />
-    <div>{props.children}</div>
+    <Navigation />                            {/* Traemos el componente Navigation */}
+    <div>{props.children}</div>               {/* Le decimos a Container que aquí deberá mostrar sus "hijos" */}
   </div>
 );
 
-export default Container;
+export default Container;                       //Exportamos por defecto, el elemento.
--- components/navigation.js
+++ components/navigation.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "next/link";   //Importamos el link de next que usaremos para rutear.
 
 const Navigation = () => {
   return (
